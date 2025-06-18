ee0b21061b45fff7434decb711ae47b6f7898830
mfarhanal <alkhiddier00@gmail.com>
2025-06-18 17:38:03 +0700

    Setup Basic Server Side

---
+++ server/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅Server Connected Successfully");
+  } catch (error) {
+    console.error("❌Connection Error", error.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
---
+++ server/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import testRouter from "./routes/testRoutes.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// testing routes
+app.use("/api/test", testRouter);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Products Management App");
+});
+
+app.listen(port, () => {
+  console.log(`🌐Server running in http://localhost:${port}`);
+});
