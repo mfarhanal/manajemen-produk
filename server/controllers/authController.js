12725cef144d234594f7f4f4c00d1ee59505c5f5
mfarhanal <alkhiddier00@gmail.com>
2025-06-21 18:01:06 +0700

    Logout Fiture

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -77,4 +77,16 @@ export const loginUser = async (req, res) => {
   }
 };
 
-export const logoutUser = async (req, res) => {};
+export const logoutUser = async (req, res) => {
+  try {
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    });
+
+    return res.json({ success: true, msg: "Log Out" });
+  } catch (error) {
+    return res.json({ success: false, msg: error.message });
+  }
+};
